717e6bc6201777d7d7abbd4348731dce5b2c20f2
anto004 <anto_004@live.com>
2018-08-30 16:52:15 -0700

    added image from url local

--- Cassini/ImageViewController.swift
+++ Cassini/ImageViewController.swift
@@ -15,12 +15,33 @@ class ImageViewController: UIViewController {
     var imageURL: URL?{
         didSet{
             imageView.image = nil;
-            fetchImage()
+            if view.window != nil { // check if you're on screen or viewDidAppear
+                fetchImage()
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated) // when you come on screen
+        if imageView.image == nil {
+            fetchImage() //make api call in background thread
         }
     }
     
     private func fetchImage(){
-        
+        if let url = imageURL {
+            let urlContents = try? Data(contentsOf: url)
+            if let imageData = urlContents {
+                imageView.image = UIImage(data: imageData)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        if imageURL == nil {
+            imageURL = DemoURLs.stanford;
+        }
     }
     
 }
